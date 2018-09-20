17450eba235152e21acba5c5eeb4a9ef9ad6186c
SugarTiger <1029109173@qq.com>
2018-09-20 23:11:12 +0800

    修改配置，兼容新版的fs.write

--- adminView/build/webpack.base.config.js
+++ adminView/build/webpack.base.config.js
@@ -81,7 +81,7 @@ module.exports = {
             id: 'happybabel',
             loaders: ['babel-loader'],
             threadPool: happyThreadPool,
-            cache: true,
+            // cache: true,
             verbose: true
         })
     ],
